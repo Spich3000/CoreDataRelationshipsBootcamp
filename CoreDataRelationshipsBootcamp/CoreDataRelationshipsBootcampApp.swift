f3befa9362195f53ba56b996f35d605ae4308656
Spich <spichakovda@gmail.com>
2022-07-20 00:30:19 +0300

    Initial Commit

---
+++ CoreDataRelationshipsBootcamp/CoreDataRelationshipsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelationshipsBootcampApp.swift
+//  CoreDataRelationshipsBootcamp
+//
+//  Created by Дмитрий Спичаков on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelationshipsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
